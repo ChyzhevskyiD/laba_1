a630f3517c699b3357f5f43b67172454e96d5117
Чижевський Дмитрій <95354969+ChyzhevskyiD@users.noreply.github.com>
2021-12-24 20:06:12 +0200

    laba_1
    laba_1

---
+++ laba_1/laba_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Laba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Num num1 = new Num("Фланець", "З", 3, 450);
+            Num num2 = new Num("Перехiдник", "К", 8, 74);
+            Num num3 = new Num("Станина", "Про", 1, 117050);
+            Console.WriteLine("Найменування Тип Кiлькiсть Вага 1 деталi(г)");
+            Console.WriteLine(num1.Details());
+            Console.WriteLine(num2.Details());
+            Console.WriteLine(num3.Details());
+        }
+    }
+    class Num
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public short number { get; set; }
+        public double weight { get; set; }
+        public Num(Num previousNum)
+        {
+            name = previousNum.name;
+            type = previousNum.type;
+            number = previousNum.number;
+            weight = previousNum.weight;
+        }
+        public Num(string Name, string Type, short Number, double Weight)
+        {
+            name = Name;
+            type = Type;
+            number = Number;
+            weight = Weight;
+        }
+        public string Details()
+        {
+            return name + "        " + type + "       " + number.ToString() + "     "
+           + weight.ToString();
+        }
+    }
+}
